e6511859e0b54babf0049c5f1fbd493a1d67d507
Сергей <Sergy87@yandex.ru>
2023-11-13 19:42:25 +0300

    hw1

---
+++ unit_test_seminar_1-main/src/main/java/task1/Calculator.java
@@ -0,0 +1,30 @@
+package task1;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divider cannot be zero");
+        }
+        return (double) a / b;
+    }
+
+    public double calculateDiscount(int a, int b) {
+        if (b < 0 || b > 100) {
+            throw new ArithmeticException("Процент скидки должен быть от 0 до 100");
+        }
+        return (double) a - a * b / 100;
+    }
+}
