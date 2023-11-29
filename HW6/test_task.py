bfb5431fe3b29767b26f5ab6b1de3fb93d9ed7ae
Сергей <Sergy87@yandex.ru>
2023-11-30 00:01:19 +0300

    add hw6

---
+++ HW6/test_task.py
@@ -0,0 +1,26 @@
+import unittest
+
+from task import ListComparator
+
+
+class test_list_comparator(unittest.TestCase):
+
+    def test_compare_lists_with_one_empty_list(self):
+        list1 = [1, 2, 3]
+        list2 = []
+        comparator = ListComparator(list1, list2)
+        assert comparator.compare_lists() == "Первый список имеет большее среднее значение"
+
+    def test_compare_lists_with_different_averages(self):
+        list1 = [1, 2, 3, 4, 5]
+        list2 = [6, 7, 8, 9, 10]
+        comparator = ListComparator(list1, list2)
+        assert comparator.compare_lists() == "Второй список имеет большее среднее значение"
+
+    def test_compare_lists_with_empty_lists(self):
+        list1 = []
+        list2 = []
+        comparator = ListComparator(list1, list2)
+        assert comparator.compare_lists() == "Средние значения равны"
+
+
